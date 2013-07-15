e88c6c2ab0e4f92139c1cedad7a641c5d8c48c08
Felix Botner <botner@univention.de>
2013-07-15 09:22:20 +0000

    changed var names (Bug #31905)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucc@42376 1b283449-9f2f-0410-b571-8eb228e1a901

--- univention-ucc-proxy-settings/conffiles/usr/lib/firefox/defaults/pref/ucc-proxy.js
+++ univention-ucc-proxy-settings/conffiles/usr/lib/firefox/defaults/pref/ucc-proxy.js
@@ -1,15 +1,23 @@
 @%@UCRWARNING=// @%@
 
 @!@
-if configRegistry.get("proxy/pac", ""):
-	print 'lockPref("network.proxy.autoconfig_url", "%s");' % configRegistry["proxy/pac"]
+if configRegistry.get("ucc/proxy/autoconfig/url", ""):
+	print 'lockPref("network.proxy.autoconfig_url", "%s");' % configRegistry["ucc/proxy/autoconfig/url"]
 	print 'lockPref("network.proxy.type", 2);'
-elif configRegistry.get("proxy/http", ""):
-	list = configRegistry["proxy/http"].split(":")
+elif configRegistry.get("ucc/proxy/http", ""):
+	list = configRegistry["ucc/proxy/http"].split(":")
 	server = ":".join(list[0:-1])
 	port = list[-1]
-	server = server.replace("http://", "")
-	print 'user_pref("network.proxy.http", "%s");' % server
-	print 'user_pref("network.proxy.http_port", %s);' % port
-	print 'user_pref("network.proxy.type", 1);'
+	if server and port:
+		server = server.replace("http://", "")
+		print 'user_pref("network.proxy.type", 1);'
+		print 'user_pref("network.proxy.http", "%s");' % server
+		print 'user_pref("network.proxy.http_port", %s);' % port
+		print 'user_pref("network.proxy.ftp", "%s");' % server
+		print 'user_pref("network.proxy.ftp_port", %s);' % port
+		print 'user_pref("network.proxy.socks", "%s");' % server
+		print 'user_pref("network.proxy.socks_port", %s);' % port
+		print 'user_pref("network.proxy.ssl", "%s");' % server
+		print 'user_pref("network.proxy.ssl_port", %s);' % port
+
 @!@
