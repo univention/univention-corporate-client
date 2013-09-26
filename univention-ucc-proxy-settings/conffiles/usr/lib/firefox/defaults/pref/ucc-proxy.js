fda84a9874b3e86087e355ab6515d4901cef066c
Felix Botner <botner@univention.de>
2013-09-26 13:20:32 +0000

    lockPref in firefox proxy default settings (Bug #32580)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucc@44510 1b283449-9f2f-0410-b571-8eb228e1a901

--- univention-ucc-proxy-settings/conffiles/usr/lib/firefox/defaults/pref/ucc-proxy.js
+++ univention-ucc-proxy-settings/conffiles/usr/lib/firefox/defaults/pref/ucc-proxy.js
@@ -4,20 +4,23 @@
 if configRegistry.get("ucc/proxy/autoconfig/url", ""):
 	print 'lockPref("network.proxy.autoconfig_url", "%s");' % configRegistry["ucc/proxy/autoconfig/url"]
 	print 'lockPref("network.proxy.type", 2);'
+	print 'lockPref("network.proxy.share_proxy_settings", true);'
+	print 'lockPref("network.proxy.no_proxies_on", "localhost, 127.0.0.1");'
 elif configRegistry.get("ucc/proxy/http", ""):
 	list = configRegistry["ucc/proxy/http"].split(":")
 	server = ":".join(list[0:-1])
 	port = list[-1]
 	if server and port:
 		server = server.replace("http://", "")
-		print 'user_pref("network.proxy.type", 1);'
-		print 'user_pref("network.proxy.http", "%s");' % server
-		print 'user_pref("network.proxy.http_port", %s);' % port
-		print 'user_pref("network.proxy.ftp", "%s");' % server
-		print 'user_pref("network.proxy.ftp_port", %s);' % port
-		print 'user_pref("network.proxy.socks", "%s");' % server
-		print 'user_pref("network.proxy.socks_port", %s);' % port
-		print 'user_pref("network.proxy.ssl", "%s");' % server
-		print 'user_pref("network.proxy.ssl_port", %s);' % port
-
+		print 'lockPref("network.proxy.type", 1);'
+		print 'lockPref("network.proxy.http", "%s");' % server
+		print 'lockPref("network.proxy.http_port", %s);' % port
+		print 'lockPref("network.proxy.ftp", "%s");' % server
+		print 'lockPref("network.proxy.ftp_port", %s);' % port
+		print 'lockPref("network.proxy.socks", "%s");' % server
+		print 'lockPref("network.proxy.socks_port", %s);' % port
+		print 'lockPref("network.proxy.ssl", "%s");' % server
+		print 'lockPref("network.proxy.ssl_port", %s);' % port
+		print 'lockPref("network.proxy.share_proxy_settings", true);'
+		print 'lockPref("network.proxy.no_proxies_on", "localhost, 127.0.0.1");'
 @!@
